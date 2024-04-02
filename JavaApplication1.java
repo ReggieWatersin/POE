74c0e75b46368d33543b22ee6adb2a399c13bf22
Ayanda Manzini <143455650+ReggieWatersin@users.noreply.github.com>
2024-04-02 08:37:03 +0000

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double num1 = 1;
+        double num2 = 2;
+        double add,sub,multi,div;
+        add = num1 + num2;
+        sub = num1 - num2;
+        multi = num1 * num2;
+        div = num1 / num2;
+                
+        System.out.println("addition " + add);
+        System.out.println("subtraction " + sub);
+        System.out.println("multiplication " + multi);
+        System.out.println("division " + div);
+    }
+    
+}
